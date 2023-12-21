0972826a390948ca75a3a30ea3bd17574aea3985
akunz19 <amanda.kunz@parkhub.com>
2023-12-21 15:01:50 -0600

    changed run script

--- run_local.sh
+++ run_local.sh
@@ -1,118 +1,161 @@
 #!/bin/bash
 
 COMMAND=$1
-export NODE_ENV=$2
-RUN=$3
-RUNVALUE=$RUN
+RUN=$2
+NODE_ENV="local"
 export LOCAL_DEPLOYMENT="true"
 
-if [ "$COMMAND" != "build" ] && [ "$COMMAND" != "build-docker" ] && [ "$COMMAND" != "start" ] && [ "$COMMAND" != "start-docker" ] && [ "$COMMAND" != "test" ] && [ "$COMMAND" != "test-docker" ] && [ "$COMMAND" != "help" ]; then
-    echo "Command not recognized"
-    exit 1
+if [ -n "$2" ]; then
+    NODE_ENV=$2
 fi
 
+export NODE_ENV
 
-if [ "$COMMAND" = "start" ] && [ -n "$RUN" ]; then
-    echo "this command is not compatible with run"
-    exit 2
+if [ -z "$COMMAND" ]; then
+    echo "Command not specified"
+    echo "run ./run_local.sh help for more information"
 fi
 
-if [ "$COMMAND" = "start-docker" ] && [ -n "$RUN" ]; then
-    echo "this command is not compatible with run"
-    exit 3
-fi
+# ---START---
 
-if [ "$COMMAND" = "test" ] && [ -n "$RUN" ]; then
-    echo "this command is not compatible with run"
-    exit 4
-fi
-
-if [ "$COMMAND" = "test-docker" ] && [ -n "$RUN" ]; then
-    echo "this command is not compatible with run"
-    exit 5
-fi
-
-if [ "$NODE_ENV" != "local" ] && [ "$NODE_ENV" != "develop" ] && [ "$NODE_ENV" != "staging" ] && [ "$NODE_ENV" != "qa" ] && [ "$NODE_ENV" != "production" ] && [ "$NODE_ENV" != "test" ]; then
-    echo "Node environment not recognized"
-    exit 6
-fi
-
-if [[ "$RUNVALUE" =~ ^.*install.*$ ]]; then
-    npm install
-    if [[ "$RUN" =~ ^.*-.*$ ]]; then
-        RUNVALUE=${RUN#install-}
+if [ -n "$COMMAND" ]; then
+  # ---ERROR HANDLING---
+    if [ "$COMMAND" != "build" ] && [ "$COMMAND" != "build-docker" ] && [ "$COMMAND" != "start" ] && [ "$COMMAND" != "start-docker" ] && [ "$COMMAND" != "test" ] && [ "$COMMAND" != "test-docker" ] && [ "$COMMAND" != "help" ] && [ "$COMMAND" != "setup-repo" ]; then
+        echo "Command not recognized"
+        exit 1
     fi
-fi
-if [ "$RUNVALUE" != "start" ] && [ "$RUNVALUE" != "start-docker" ] && [ "$RUNVALUE" != "test" ] && [ "$RUNVALUE" != "test-docker" ] && [ "$RUN" != "install" ]; then
-    echo "Run not recognized"
-    exit 7
-fi
 
-if [ "$COMMAND" = "build" ]; then
-    chmod +x ./scripts/setup.sh
-    source ./scripts/setup.sh
-    npm run build
-fi
+    if [ "$COMMAND" != "build" ] && [ "$COMMAND" != "build-docker" ] && [ -n "$RUN" ]; then
+        echo "Run is not compatible with this command"
+        exit 2
+    fi
 
-if [ "$COMMAND" = "build-docker" ]; then
-    chmod +x ./scripts/compose.sh
-    source ./scripts/compose.sh
-fi
+    if [ "$COMMAND" = "setup-repo" ] && [ "$RUN" = "start-docker" ]; then
+        echo "setup-repo command is not compatible with start-docker, please use start instead"
+        exit 3
+    fi
 
-if [ "$COMMAND" = "start" ] && [ -z "$RUN" ]; then
-    npm run start
-fi
+    if [ "$COMMAND" = "setup-repo" ] && [ "$RUN" = "test-docker" ]; then
+        echo "setup-repo command is not compatible with test-docker, please use test instead"
+        exit 4
+    fi
 
-if [ "$COMMAND" = "start-docker" ] && [ -z "$RUN" ]; then
-    docker-compose run attache-rest-api npm start
-fi
+    if [ "$COMMAND" = "build-docker" ] && [ "$RUN" = "start" ]; then
+        echo "build-docker command is not compatible with start, please use start-docker instead"
+        exit 5
+    fi
 
-if [ "$COMMAND" = "test" ] && [ -z "$RUN" ]; then
-    npm run test
-fi
+    if [ "$COMMAND" = "build-docker" ] && [ "$RUN" = "test" ]; then
+        echo "build-docker command is not compatible with test, please use test-docker instead"
+        exit 6
+    fi
 
-if [ "$COMMAND" = "test-docker" ] && [ -z "$RUN" ]; then
-    docker-compose run attache-rest-api npm run test
-fi
+    if [ "$COMMAND" = "build" ] && [ "$RUN" = "start-docker" ]; then
+        echo "build command is not compatible with start-docker, please use start instead"
+        exit 7
+    fi
 
-if [ "$COMMAND" = "help" ]; then
-    echo "Usage: source ./build_local.sh [command] [node_env] [run]"
-    echo "Commands:"
-    echo "  build: build the app"
-    echo "  build-docker: build the app in docker"
-    echo "  start: start the app"
-    echo "  start-docker: start the app in docker"
-    echo "  test: run tests"
-    echo "  test-docker: run tests in docker"
-    echo "  help: display this message"
-    echo "Node Environments: this determines which environment to run locally"
-    echo "  local: local development"
-    echo "  develop: development"
-    echo "  staging: staging"
-    echo "  qa: qa"
-    echo "  production: production"
-    echo "  test: test"
-    echo "Run: pass in a value to run the app and/or install dependencies when the command value is build or build-docker"
-    echo "  install: install dependencies"
-    echo " you can prefix any of the below run values with install- to install dependencies"
-    echo "  start: start the app"
-    echo "  start-docker: start the app in docker"
-    echo "  test: run tests"
-    echo "  test-docker: run tests in docker"
-fi
+    if [ "$COMMAND" = "build" ] && [ "$RUN" = "test-docker" ]; then
+        echo "build command is not compatible with test-docker, please use test instead"
+        exit 8
+    fi
 
-if [ "$RUNVALUE" = "start" ]; then
-    npm run start
-fi
+    if [ "$NODE_ENV" != "local" ] && [ "$NODE_ENV" != "develop" ] && [ "$NODE_ENV" != "staging" ] && [ "$NODE_ENV" != "qa" ] && [ "$NODE_ENV" != "production" ] && [ "$NODE_ENV" != "test" ]; then
+        echo "Node environment not recognized"
+        exit 9
+    fi
 
-if [ "$RUNVALUE" = "start-docker" ]; then
-    docker-compose run attache-rest-api npm start
-fi
+    if [ -n "$RUN" ]; then
+        if [ "$RUN" != "start" ] && [ "$RUN" != "start-docker" ] && [ "$RUN" != "test" ] && [ "$RUN" != "test-docker" ]; then
+            echo "Run not recognized"
+            exit 10
+        fi
+    fi
+    # ---END ERROR HANDLING---
 
-if [ "$RUNVALUE" = "test" ]; then
-    npm run test
-fi
+    # ---COMMAND---
+    if [ "$COMMAND" = "setup-repo" ]; then
+        npm install
+        if [ ! -d "./dist" ]; then
+            mkdir dist
+        fi
+        chmod +x ./scripts/setup.sh
+        source ./scripts/setup.sh
+        npm run build
+    fi
 
-if [ "$RUNVALUE" = "test-docker" ]; then
-    docker-compose run attache-rest-api npm run test
+    if [ "$COMMAND" = "build" ]; then
+        chmod +x ./scripts/setup.sh
+        source ./scripts/setup.sh
+        npm run build
+    fi
+
+    if [ "$COMMAND" = "build-docker" ]; then
+        chmod +x ./scripts/compose.sh
+        source ./scripts/compose.sh
+    fi
+
+    if [ "$COMMAND" = "start" ] && [ -z "$RUN" ]; then
+        npm run start
+    fi
+
+    if [ "$COMMAND" = "test" ] && [ -z "$RUN" ]; then
+        npm run test
+    fi
+
+    if [ "$COMMAND" = "start-docker" ] && [ -z "$RUN" ]; then
+        docker-compose run attache-rest-api npm start
+    fi
+
+    if [ "$COMMAND" = "test-docker" ] && [ -z "$RUN" ]; then
+        docker-compose run attache-rest-api npm run test
+    fi
+
+    if [ "$COMMAND" = "help" ]; then
+        echo "Usage: source ./run_local.sh [command] [node_env] [run]"
+        echo "Commands:"
+        echo "  build: build the app"
+        echo "  build-docker: build the app in docker"
+        echo "  start: start the app"
+        echo "  start-docker: start the app in docker"
+        echo "  test: run tests"
+        echo "  test-docker: run tests in docker"
+        echo "  setup-repo: setup the repo"
+        echo "  help: display this message"
+        echo "Node Environments: this determines which environment to run locally"
+        echo "  local: local development"
+        echo "  develop: development"
+        echo "  staging: staging"
+        echo "  qa: qa"
+        echo "  production: production"
+        echo "  test: test"
+        echo "Run: pass in a value to run the app when the command value is build, build-docker, or setup-repo"
+        echo "  start: start the app"
+        echo "  start-docker: start the app in docker"
+        echo "  test: run tests"
+        echo "  test-docker: run tests in docker"
+    fi
+  # ---END COMMAND---
+
+  # ---RUN---
+  if [ -n "$RUN" ]; then
+    if [ "$RUN" = "start" ]; then
+        npm run start
+    fi
+
+    if [ "$RUN" = "start-docker" ]; then
+        docker-compose run attache-rest-api npm start
+    fi
+
+    if [ "$RUN" = "test" ]; then
+        npm run test
+    fi
+
+    if [ "$RUN" = "test-docker" ]; then
+        docker-compose run attache-rest-api npm run test
+    fi
+  fi
+  # ---END RUN---
+
 fi
+# ---END---
