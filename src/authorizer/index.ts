095a378247efaed6ef3f127c84526acbc6f44eb6
akunz19 <amanda.kunz@parkhub.com>
2024-01-17 12:40:36 -0600

    fixing env casing

--- src/authorizer/index.ts
+++ src/authorizer/index.ts
@@ -1,36 +1,36 @@
-'use strict'
+'use strict';
 
 import { Request, Response, NextFunction } from 'express';
 
 const basicAuth = (req: Request, res: Response, next: NextFunction) => {
-    // The expected credentials.
-    const { headers, path } = req
-    const route = path.split('/')[2]
-    const APP = `${process.env[`${route.toUpperCase()}_APP`]}`;
-    const SECRET = `${process.env[`${route.toUpperCase()}_SECRET`]}`;
-    // Extract the auth header
-    const authHeader = headers.authorization;
+	// The expected credentials.
+	const { headers, path } = req;
+	const route = path.split('/')[2];
+	const APP = `${process.env[`${route.toLowerCase()}_app`]}`;
+	const SECRET = `${process.env[`${route.toLowerCase()}_secret`]}`;
+	// Extract the auth header
+	const authHeader = headers.authorization;
 
-    if (authHeader) {
-        // Split on a space, the original authHeader looks like "Basic encodedstring"
-        const [scheme, encoded] = authHeader.split(' ');
+	if (authHeader) {
+		// Split on a space, the original authHeader looks like "Basic encodedstring"
+		const [scheme, encoded] = authHeader.split(' ');
 
-        if (scheme === 'Basic') {
-            // Decode the base64 string
-            const decoded = Buffer.from(encoded, 'base64').toString('utf-8');
-            const [app, secret] = decoded.split(':');
+		if (scheme === 'Basic') {
+			// Decode the base64 string
+			const decoded = Buffer.from(encoded, 'base64').toString('utf-8');
+			const [app, secret] = decoded.split(':');
 
-            // Check username and password against the expected credentials
-            if (app === APP && secret === SECRET) {
-                // Authentication successful
-                return next();
-            }
-        }
-    }
+			// Check username and password against the expected credentials
+			if (app === APP && secret === SECRET) {
+				// Authentication successful
+				return next();
+			}
+		}
+	}
 
-    // Authentication failed
-    res.setHeader('WWW-Authenticate', 'Basic');
-    return res.status(401).json({ message: 'Authentication failed' });
+	// Authentication failed
+	res.setHeader('WWW-Authenticate', 'Basic');
+	return res.status(401).json({ message: 'Authentication failed' });
 };
 
 export default basicAuth;
