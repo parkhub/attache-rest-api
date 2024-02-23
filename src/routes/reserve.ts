d2168b9fc3ca6786389f611233f74852c84f6357
akunz19 <amanda.kunz@parkhub.com>
2024-02-23 11:21:41 -0600

    test

--- src/routes/reserve.ts
+++ src/routes/reserve.ts
@@ -5,110 +5,109 @@ import { attacheClient, dataClient } from '../attache';
 import {
 	DeleteReservationRequest,
 	PostReservationRequest,
-	PutReservationRequest,
 	ReserveResponse
 } from '../types/reserve';
-import {validateRequired, validateOptional, validateCreateOrChange, validatePost, validatePut, validateDelete} from './reserve/validate';
+import {validateRequired, validateOptional, validateCreateOrChange, validatePost, validateDelete} from './reserve/validate';
 import { postHandler } from './reserve/handlers';
 import { AttacheError, Source } from '../types/attache';
 const router = Router();
 
-router.post('/', async (req: PostReservationRequest, res: Response) => {
-	const pass = req.body;
+// router.post('/', async (req: PostReservationRequest, res: Response) => {
+// 	const pass = req.body;
 
-	try {
-		validateRequired(pass);
-		validateOptional(pass);
-		validateCreateOrChange(pass);
-		validatePost(pass);
-	} catch (err) {
-		const error = err as Error;
-		res.status(400)
-			.json({ result: 'invalid', message: error.message, reject: true });
-		return;
-	}
-	try {
-		const response = await postHandler(pass);
-		delete response.reservation?.code;
-		delete response.reservation?.description;
-		if (response.result !== 'valid' && response.reject) {
-			res.status(400).json(response);
-			return;
-		}
-		res.status(200).json(response);
-	} catch (err) {
-		console.error(err); //TODO change to signal logger
-		res.status(500).json({result: 'failed', reject: true, message: 'Internal Server Error'});
-		return;
-	}
-});
+// 	try {
+// 		validateRequired(pass);
+// 		validateOptional(pass);
+// 		validateCreateOrChange(pass);
+// 		validatePost(pass);
+// 	} catch (err) {
+// 		const error = err as Error;
+// 		res.status(400)
+// 			.json({ result: 'invalid', message: error.message, reject: true });
+// 		return;
+// 	}
+// 	try {
+// 		const response = await postHandler(pass);
+// 		delete response.reservation?.code;
+// 		delete response.reservation?.description;
+// 		if (response.result !== 'valid' && response.reject) {
+// 			res.status(400).json(response);
+// 			return;
+// 		}
+// 		res.status(200).json(response);
+// 	} catch (err) {
+// 		console.error(err); //TODO change to signal logger
+// 		res.status(500).json({result: 'failed', reject: true, message: 'Internal Server Error'});
+// 		return;
+// 	}
+// });
 
-router.put('/', async (req: PutReservationRequest, res: Response) => {
-	const pass = req.body;
-	try {
-		validateRequired(pass);
-		validateOptional(pass);
-		validateCreateOrChange(pass);
-		validatePut(pass);
-	} catch (err) {
-		console.error(err); //TODO change to signal logger
-		res.status(400).json({
-			result: 'invalid',
-			message: (err as Error).message,
-			reject: true,
-		});
-		return;
-	}
-	try {
-		const response = await attacheClient()
-			.reserve()
-			.handler({ pass })
-			.update();
+// router.put('/', async (req: PutReservationRequest, res: Response) => {
+// 	const pass = req.body;
+// 	try {
+// 		validateRequired(pass);
+// 		validateOptional(pass);
+// 		validateCreateOrChange(pass);
+// 		validatePut(pass);
+// 	} catch (err) {
+// 		console.error(err); //TODO change to signal logger
+// 		res.status(400).json({
+// 			result: 'invalid',
+// 			message: (err as Error).message,
+// 			reject: true,
+// 		});
+// 		return;
+// 	}
+// 	try {
+// 		const response = await attacheClient()
+// 			.reserve()
+// 			.handler({ pass })
+// 			.update();
 
-		if (response.result !== 'updated' && response.reject) {
-			res.status(400).send(JSON.stringify(response, null, 4));
-			return;
-		}
-		res.status(200).json(response);
-	} catch (err) {
-		console.error(err); //TODO change to signal logger
-		res.status(500).json({result: 'failed', reject: true, message: 'Internal Server Error'});
-		return;
-	}
-});
+// 		if (response.result !== 'updated' && response.reject) {
+// 			res.status(400).send(JSON.stringify(response, null, 4));
+// 			return;
+// 		}
+// 		res.status(200).json(response);
+// 	} catch (err) {
+// 		console.error(err); //TODO change to signal logger
+// 		res.status(500).json({result: 'failed', reject: true, message: 'Internal Server Error'});
+// 		return;
+// 	}
+// });
 
-router.delete('/', async (req: DeleteReservationRequest, res: Response) => {
-	const pass = req.body;
-	try {
-		validateRequired(pass);
-		validateOptional(pass);
-		validateDelete(pass);
-	} catch (err) {
-		console.error(err); //TODO change to signal logger
-		res.status(400).json({
-			result: 'invalid',
-			message: (err as Error).message,
-			reject: true,
-		});
-		return;
-	}
-	try {
-		const response = await attacheClient()
-			.reserve()
-			.handler({ pass })
-			.cancel() as ReserveResponse;		
-		delete response.reservation?.code;
-		delete response.reservation?.description;
-		if (response.result !== 'cancelled' && response.reject) {
-			res.status(400).json(response);
-		}
-		res.status(200).json(response);
-	} catch (err) {
-		console.error(err); //TODO change to signal logger
-		res.status(500).json({result: 'failed', reject: true, message: 'Internal Server Error'});
-		return;
-	}
-});
+// router.delete('/', async (req: DeleteReservationRequest, res: Response) => {
+// 	const pass = req.body;
+// 	try {
+// 		validateRequired(pass);
+// 		validateOptional(pass);
+// 		validateDelete(pass);
+// 	} catch (err) {
+// 		console.error(err); //TODO change to signal logger
+// 		res.status(400).json({
+// 			result: 'invalid',
+// 			message: (err as Error).message,
+// 			reject: true,
+// 		});
+// 		return;
+// 	}
+// 	try {
+// 		const response = await attacheClient()
+// 			.reserve()
+// 			.handler({ pass })
+// 			.cancel() as ReserveResponse;		
+// 		delete response.reservation?.code;
+// 		delete response.reservation?.description;
+// 		if (response.result !== 'cancelled' && response.reject) {
+// 			res.status(400).json(response);
+// 		}
+// 		res.status(200).json(response);
+// 	} catch (err) {
+// 		console.error(err); //TODO change to signal logger
+// 		res.status(500).json({result: 'failed', reject: true, message: 'Internal Server Error'});
+// 		return;
+// 	}
+// });
 
 router.post('/smartpass', async (req: PostReservationRequest, res: Response) => {
 	const pass = req.body;
